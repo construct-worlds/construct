b1851ea25c36ad95171917cf1a6550c76ef77f22
Edwin <43801454+edwin-zvs@users.noreply.github.com>
2026-08-04 07:13:19 -0700

    chore(release): bump version to 0.17.1 (#1209)

--- Formula/construct.rb
+++ Formula/construct.rb
@@ -5,7 +5,7 @@
 class Construct < Formula
   desc "Terminal-native agentic development environment"
   homepage "https://github.com/construct-worlds/construct"
-  version "0.17.0"
+  version "0.17.1"
   license "MIT"
 
   depends_on :macos
