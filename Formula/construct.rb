e7db7c025fc97f008ec8b1215041b67eccd21c29
Edwin <43801454+edwin-zvs@users.noreply.github.com>
2026-09-03 08:07:47 -0700

    release: v0.17.13 (#1324)

--- Formula/construct.rb
+++ Formula/construct.rb
@@ -5,7 +5,7 @@
 class Construct < Formula
   desc "Terminal-native agentic development environment"
   homepage "https://github.com/construct-worlds/construct"
-  version "0.17.12"
+  version "0.17.13"
   license "MIT"
 
   depends_on :macos
